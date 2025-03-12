e8186985e79cac04923c305571c7a52481d634bb
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-12 14:57:59 +0200

    My journey to learn C++

---
+++ C++/Basic/cin && getline.cpp
@@ -0,0 +1,17 @@
+// string  (cin && getline).....
+#include <iostream>
+#include <string>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+
+    string x ="salah"  ;
+    getline(cin , x );  
+    cout<<"x is = "<< x <<endl;   // salah hassan   
+
+    return 0 ;
+}
+
+
