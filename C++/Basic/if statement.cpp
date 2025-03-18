24d92f61f9bed5f030b185b8d2ea467749a6fb29
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-18 21:15:02 +0200

    My journey to learn C++

---
+++ C++/Basic/if statement.cpp
@@ -0,0 +1,31 @@
+// if statement 
+
+#include <iostream>
+using namespace std;
+int main(){
+int x=10;
+
+if (x>=2)
+{
+cout<< "x is greater than than two "<< endl;
+}
+else{
+cout<< "thank you"<<endl;
+}
+
+
+
+//! (x>=2)?cout<< "x is greater than than two "<< endl:cout<< "thank you"<<endl;
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
