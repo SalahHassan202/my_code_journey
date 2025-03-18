033025a9eab4ea686f8845585f1f3a6ad7692d79
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-18 21:10:05 +0200

    My journey to learn C++

---
+++ C++/Basic/declare array.cpp
@@ -0,0 +1,35 @@
+// declare array 
+# include <iostream>
+#include <bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+// declare array 
+  
+
+// first method 
+int x[5]={1,2,3,4,5};
+
+
+// second method 
+int y[]={1,2,3,4,5};
+
+
+// thired method 
+int z[5];
+
+
+
+
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
