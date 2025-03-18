ddaa49d56497879f5e92f7e46a3e5f595236211e
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-18 21:15:27 +0200

    My journey to learn C++

---
+++ C++/Basic/inLine if .cpp
@@ -0,0 +1,24 @@
+// inLine if 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x =5 ;
+    /*
+    
+    if( x == 5 )
+    {
+        cout << "YES" <<endl;
+    }
+    else
+    {
+        cout << "NO" <<endl;
+    }
+
+    */
+
+   cout<<(x == 5 ? "YES" : "NO") ;
+    
+    return 0;
+}
