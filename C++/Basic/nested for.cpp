ef6cc372162efa4035bb9538806b0c6df644a8cf
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-18 21:16:28 +0200

    My journey to learn C++

---
+++ C++/Basic/nested for.cpp
@@ -0,0 +1,19 @@
+//  Nested for
+#include <iostream>
+using namespace std;
+int main()
+{
+   for(int x =0 ; x<=1 ; x++)
+   {
+    
+    for (int y =1 ; y <=3 ; y++)
+    {
+        cout<<"1"<<endl;
+    }
+    
+    cout <<"Salah Hassan"<<endl;
+   }
+
+    
+    return 0;
+}
