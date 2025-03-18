d0bfbfff039d54600e3cdec491fd3cb1ba3f2427
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-18 21:10:32 +0200

    My journey to learn C++

---
+++ C++/Basic/def of array.cpp
@@ -0,0 +1,29 @@
+// array 
+# include <iostream>
+#include <bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+int x=21 ;
+int y=14 ;
+int z=11 ;
+int w=9 ;
+int d=2 ;
+int c=3 ;
+// def of array
+
+int x[6]={21,14,11,9,2,3};
+
+
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
