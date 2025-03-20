6b699e3d6b23994b7c41359fca45ea53f611c63a
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-20 17:55:23 +0200

    My journey to learn C++

---
+++ C++/Basic/for loop exe.cpp
@@ -0,0 +1,17 @@
+//  for loop 
+// print 10 9 8 7 6 5 4 3 2 1 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+ for (int i =10;i>=1;)
+ {
+ cout << i<<endl;
+ i--;
+ }
+
+ 
+
+    return 0;
+}
