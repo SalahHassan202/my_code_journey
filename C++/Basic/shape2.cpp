4f9cdc21dbd200c219171c94df23f38ce55ca2e8
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-22 23:25:54 +0200

    My journey to learn C++

---
+++ C++/Basic/shape2.cpp
@@ -0,0 +1,25 @@
+// shapes.......
+#include <iostream>
+#include <string>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+ // triangle
+    for(int i =1 ; i<= 5 ; i++)
+    {
+        for( int j =1 ; j<= i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+
+
+
+   return 0 ;
+}
+
+
