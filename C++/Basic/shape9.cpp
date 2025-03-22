af6245af0dfcf94256e18ce081c2bfea648595a1
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-22 23:29:22 +0200

    My journey to learn C++

---
+++ C++/Basic/shape9.cpp
@@ -0,0 +1,30 @@
+// shapes.......
+#include <iostream>
+#include <string>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+     for(int i =1 ; i<=5 ; i++)
+     {
+        for(int j =1 ; j<=5 ; j++)
+        {
+            if (i ==j  || j ==6 -i)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+
+        }
+        cout<<endl;
+     }       
+
+    
+   return 0 ;
+}
+
+
