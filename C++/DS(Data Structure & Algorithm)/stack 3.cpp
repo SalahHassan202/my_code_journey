d5be46a705d20029cd241a0293f159e4ef1c47e9
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-23 23:08:29 +0200

    My journey to learn C++

---
+++ C++/DS(Data Structure & Algorithm)/stack 3.cpp
@@ -0,0 +1,19 @@
+// stack
+# include <iostream>
+#include <stack>
+using namespace std ;
+int main()
+{
+   // create a stack of strings
+    stack <string> languages ;
+    
+    // add element to the Stack
+    languages.push("C++");
+    languages.push("Java");
+    languages.push("Python");
+    
+    // print top element
+    cout << languages.top();           // python 
+
+    return  0 ; 
+} 
