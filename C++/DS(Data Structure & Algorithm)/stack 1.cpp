2a29735749169b7001e5fbdd8238950bee3e1575
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-23 23:07:31 +0200

    My journey to learn C++

---
+++ C++/DS(Data Structure & Algorithm)/stack 1.cpp
@@ -0,0 +1,15 @@
+// stack
+# include <iostream>
+#include <stack>
+using namespace std ;
+int main()
+{
+    stack <int> nums ;
+    // check for empty stack or no..?
+    cout << "Empty : " << nums.empty() << endl;
+
+    // 1 (true) - if the stack is empty
+    // 0(false) - if the stack is not empty
+
+    return  0 ; 
+}
