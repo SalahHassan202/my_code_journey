91c612f36433c8ed50b657d1af6d929765e6996c
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-31 18:41:07 +0200

    My journey to learn C++

---
+++ C++/Basic/pointer 1.cpp
@@ -0,0 +1,13 @@
+// pointer
+#include <iostream>
+using namespace std;
+// pointer ===> متغير بيخزن قيمة لعنوان مكان في الميموري
+int main()
+{
+
+    int x = 20 ;
+    cout << x << endl ;                // 20 
+    cout << &x << endl;               // 0x61ff0c
+    
+    return 0 ;
+}
