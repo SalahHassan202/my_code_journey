af6567760366a752de329fb9c6b1cd7171b6a710
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-31 18:44:02 +0200

    My journey to learn C++

---
+++ C++/Basic/pointer 7.cpp
@@ -0,0 +1,13 @@
+// pointer
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int * myPointer = NULL ;                      //  or ===>        int * myPointer = nullptr ;               
+    cout<< myPointer << endl;              // 0 
+
+    // كدا البوينتر مش بيشاور علي اي حاجه خالص 
+
+    return 0 ;
+}
