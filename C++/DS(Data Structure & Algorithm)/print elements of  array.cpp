cd7eb979949f12b0d736e47d446aa1a9164c6049
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-01 05:13:32 +0200

    My journey to learn C++

---
+++ C++/DS(Data Structure & Algorithm)/print elements of  array.cpp
@@ -0,0 +1,28 @@
+// print elements of  array 
+# include <iostream>
+#include <bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+
+//   int x[4]={1,2,3,4,5};            //error 
+
+
+int x[4]={11,20,35,14};   
+cout<< x[0]<<endl;
+cout<< x[1]<<endl;
+cout<< x[2]<<endl;
+cout<< x[3]<<endl;
+
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
