775690b6f61956dea92c00f6c1a21c3188e4a32d
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-01 05:11:32 +0200

    My journey to learn C++

---
+++ C++/DS(Data Structure & Algorithm)/Two Dimensional Array.cpp
@@ -0,0 +1,39 @@
+// print elements of  array 
+# include <iostream>
+#include <bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+
+// Two Dimensional Array 
+
+int a[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};     // int a[3][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+
+
+cout<<a[0][0]<<endl;      //1
+cout<<a[0][1]<<endl;      //2
+cout<<a[0][2]<<endl;      //3
+cout<<a[0][3]<<endl;      //4
+cout<<a[1][0]<<endl;      //5
+cout<<a[1][1]<<endl;      //6
+cout<<a[1][2]<<endl;      //7
+cout<<a[1][3]<<endl;      //8
+cout<<a[2][0]<<endl;      //9
+cout<<a[2][1]<<endl;      //10
+cout<<a[2][2]<<endl;      //11
+cout<<a[2][3]<<endl;      //12
+
+
+
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
