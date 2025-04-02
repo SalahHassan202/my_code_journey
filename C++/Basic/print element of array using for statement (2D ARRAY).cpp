f9b08113f34d3225031368b574253971186cd4d0
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-02 23:51:54 +0200

    My journey to learn C++

---
+++ C++/Basic/print element of array using for statement (2D ARRAY).cpp
@@ -0,0 +1,35 @@
+// print element of array using for statement (2D ARRAY)
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+
+
+//print element of array using for statement (2D ARRAY)
+
+
+int x[2][3]={{41,30,17},{26,11,90}};
+// i == row   &&   j == coulmn
+for(int i=0 ; i<2 ;i++)
+{
+    for(int j=0 ; j<3 ;j++)
+    {
+        cout<<x[i][j]<<endl;
+    }
+}
+
+
+
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
