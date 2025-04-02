a44787932b38ee728915ff8e992f7533f0448e71
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-02 23:49:22 +0200

    My journey to learn C++

---
+++ C++/Basic/print statement.cpp
@@ -0,0 +1,27 @@
+// Write a program in C++ to print welcome text on a separate line.
+// ====================================================================
+// Sample Output:
+
+// Print a welcome text in a separate line :                             
+// ----------------------------------------------                         
+// Welcome to                                                            
+//  w3resource.com
+
+// Exercise-1 with Solution
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm> 
+using namespace std ;
+
+int main ()
+{
+
+    cout << "Print a welcome text in a separate line :"<< endl;
+    cout << "---------------------------------------------- " << endl;
+    cout << "Welcome to " << endl;
+    cout << "w3resource.com" <<endl;
+
+
+    return 0 ;
+}
