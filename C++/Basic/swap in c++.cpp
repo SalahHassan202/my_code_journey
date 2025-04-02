e00371e8db3550925af6314d16396ccbdd1b9437
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-02 23:43:58 +0200

    My journey to learn C++

---
+++ C++/Basic/swap in c++.cpp
@@ -0,0 +1,22 @@
+//  swap in c++
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int x=10 ; 
+int y =20 ;
+int temp ;
+
+temp =x ;   // temp =10 ;
+x =y;       // x =20 ;
+y =temp ;   // y = 10 ;
+
+// this is called swap between to number 
+ 
+
+cout<<"x is = "<<x<<endl;
+cout<<"y is = "<<y<<endl;
+
+    return 0 ;
+}
