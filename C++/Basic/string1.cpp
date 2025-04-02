d9ad13f5938a92c61691c1843659ee46795be18e
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-02 23:42:40 +0200

    My journey to learn C++

---
+++ C++/Basic/string1.cpp
@@ -0,0 +1,17 @@
+// string.....
+#include <iostream>
+#include <string>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+
+    string x ="salah" ,y ;
+    y =x ;
+    cout<<"y is = "<<y <<endl;
+
+    return 0 ;
+}
+
+
