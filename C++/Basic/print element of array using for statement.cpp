d4d70e796c8ee8c23f3bcd7c00be60f757e2bdd2
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-02 23:50:49 +0200

    My journey to learn C++

---
+++ C++/Basic/print element of array using for statement.cpp
@@ -0,0 +1,31 @@
+// print element of array using for statement
+# include <iostream>
+#include <bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+
+
+// print element of array using for statement
+
+
+int x[3]={92,43,21};
+
+for(int i=0 ;i<3;i++)
+{
+    cout<<x[i]<<endl;
+}
+
+
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
