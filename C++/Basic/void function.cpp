922707cb862888593d11bd5b1c4978f4a339a839
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-02 23:48:25 +0200

    My journey to learn C++

---
+++ C++/Basic/void function.cpp
@@ -0,0 +1,18 @@
+//  functions......
+#include <iostream>
+using namespace std;
+
+//  void function  & return 
+
+void print (int x , int y )
+{
+    cout<<"x+y = "<<x+y <<endl;
+}
+int main()
+{
+  
+  print (5, 2) ;
+   
+    
+    return 0;
+}
