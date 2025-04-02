399ae9be88f652a90195bf2d9f322e95f6aab4eb
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-02 23:53:55 +0200

    My journey to learn C++

---
+++ C++/Basic/random function.cpp
@@ -0,0 +1,35 @@
+//  random functions......
+#include <iostream>
+using namespace std;
+int main()
+{
+ for(int i =0 ; i<10 ; i++)
+ {
+
+    cout<< rand()<<endl;    //  random function 
+ }
+
+    return 0 ;
+}
+
+
+/*
+
+
+
+//////// output /////////
+
+41
+18467
+6334
+26500
+19169
+15724
+11478
+29358
+26962
+24464
+
+
+
+*/
