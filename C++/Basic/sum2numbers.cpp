7bb30273a4884b5f17db6978a5638b2e9b7dda15
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-02 23:43:30 +0200

    My journey to learn C++

---
+++ C++/Basic/sum2numbers.cpp
@@ -0,0 +1,25 @@
+// Write a program in C++ to print the sum of two numbers.
+// ============================================================
+// Sample Output:
+
+// Print the sum of two numbers :                                        
+// -----------------------------------                                    
+// The sum of 29 and 30 is : 59
+
+// Exercise-2 with Solution
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm> 
+using namespace std ;
+
+int main ()
+{
+
+    cout << "Print the sum of two numbers : "<< endl;
+    cout << "---------------------------------------------- " << endl;
+    cout << "The sum of 29 and 30 is : "<< 29+30 <<endl;
+
+
+    return 0 ;
+}
