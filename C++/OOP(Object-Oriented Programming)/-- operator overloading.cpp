1e7e6a714c7c908939cb6c3cc42f078f7cb01184
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-03 23:35:18 +0200

    My journey to learn C++

---
+++ C++/OOP(Object-Oriented Programming)/-- operator overloading.cpp
@@ -0,0 +1,43 @@
+// operator overloading...... (OPP)
+// -- operator overloading
+#include <iostream>
+#include <string >
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+class Test
+{
+    private :
+        int x = 0 ;
+    public :
+        Test()
+        {
+            cout << "enter x : " ;
+            cin >> x ;
+        }
+        void operator --(int)
+        {
+            x-- ;      // x = x - 1                   // postfix // --x 
+        }
+        void operator --()
+        {
+            --x ;      // x = x - 1                   // prefix // --x 
+        }
+        void print()
+        {
+            cout << "x is : " << x << endl;
+        }
+} ;
+
+int main ()
+{      
+    Test ob1 ;
+    ob1-- ;        // postfix
+    --ob1 ;        // prefix
+    ob1.print() ;
+
+
+    return 0 ;
+}
+
