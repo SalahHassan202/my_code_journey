4b759d6600cce246ea823d189c8f5fc7de2c1766
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-03 23:33:29 +0200

    My journey to learn C++

---
+++ C++/Basic/repeat my name 100 times.cpp
@@ -0,0 +1,15 @@
+//  for loop 
+// reapet my name 100 times 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+ 
+ for (int i=1 ; i<=100;i++)
+ {
+    cout <<i<< " :Salah Hassan  \n";
+ }
+
+    return 0;
+}
