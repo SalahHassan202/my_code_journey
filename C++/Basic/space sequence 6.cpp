7bef1350c070c905ed29b41a97805c6b5826862c
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-03 23:32:10 +0200

    My journey to learn C++

---
+++ C++/Basic/space sequence 6.cpp
@@ -0,0 +1,14 @@
+// space sequence 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello world! \n" ;             // new line 
+    cout << "hello world!"<< endl;          // new line 
+  
+    
+
+    return 0 ;
+}
+ 
